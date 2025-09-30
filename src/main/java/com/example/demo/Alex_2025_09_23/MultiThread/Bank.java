df1eda9be6024897f20ce1b901c7413ebd0e18d3
Scherban Aleksey <aleksey.scherban@gmail.com>
2025-09-30 18:26:00 +0300

    Решенный банкомат

---
+++ src/main/java/com/example/demo/Alex_2025_09_23/MultiThread/Bank.java
@@ -0,0 +1,102 @@
+package com.example.demo.Alex_2025_09_23.MultiThread;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Bank {
+
+    public int[] nominals = {5000, 2000, 1000, 500, 200, 100};
+    public int[] quontityNominals = {5, 5, 5, 5, 5, 0};
+
+
+    public int[] getNominals() {
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите сумму для снятия ");
+        int originalTake = sc.nextInt();
+
+
+        int totalSum = 0;
+        for (int i = 0; i < nominals.length; i++) {
+            totalSum += nominals[i] * quontityNominals[i];
+
+        }
+        System.out.println("Всего денег " + totalSum);
+
+
+        if (originalTake > totalSum) {
+            System.out.println(" Запрошена слишком большая сумма");
+            return null;
+        }
+
+        int[] issuedNominals = new int[nominals.length];
+        boolean success = false;
+        int[] currentIssued = new int[nominals.length];
+        int take = originalTake;
+
+
+        for (int j = 0; j < nominals.length; j++) {
+
+            System.out.println("j= " + j);
+
+            System.out.println("take= " + take);
+
+
+            int[] currentQuantity = quontityNominals.clone();
+            originalTake = take;
+
+            for (int i = j; i < nominals.length; i++) {
+                while (take >= nominals[i] && currentQuantity[i] > 0) {
+                    take -= nominals[i];
+                    quontityNominals[i]--;
+                    currentIssued[i]++;
+                }
+                System.out.println(Arrays.toString(currentIssued));
+
+            }
+            int x = currentIssued[j] - 1;
+            System.out.println("После внутреннего цикла " + x);
+
+            if (take == 0) {
+
+                quontityNominals = currentQuantity;
+                issuedNominals = currentIssued;
+                success = true;
+                System.out.println("Успех с j = " + j);
+                break;
+            } else {
+                currentIssued[j] = x;
+                for (int k = j + 1; k < nominals.length; k++) {
+                    currentIssued[k] = 0;
+                }
+                take = originalTake - currentIssued[j] * nominals[j];
+                System.out.println(Arrays.toString(currentIssued));
+                System.out.println(take);
+            }
+        }
+
+        if (!success) {
+            System.out.println("Не удалось выдать сумму " + originalTake);
+            return new int[nominals.length];
+        }
+
+
+        for (int i = 0; i < nominals.length; i++) {
+            System.out.println("Выдано " + nominals[i] + "  " + issuedNominals[i]);
+        }
+
+        return issuedNominals;
+
+    }
+
+    public static void main(String[] args) {
+
+        Bank bank = new Bank();
+
+        bank.getNominals();
+
+
+    }
+
+}
